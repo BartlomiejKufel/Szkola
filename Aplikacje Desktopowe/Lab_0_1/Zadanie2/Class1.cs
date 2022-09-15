4174f91a260709b5dd49ea13724eb69a70b35209
Bartłomiej Kufel <joloczok123@gmail.com>
2022-09-15 08:35:07 +0200

    Dodanie Lab_0_1

---
+++ Aplikacje Desktopowe/Lab_0_1/Zadanie2/Class1.cs
@@ -0,0 +1,32 @@
+﻿class Prostopadloscian
+{
+    //właściwości
+    public double Height { get; set; }
+    public double Width { get; set; }   
+    public double Length { get; set; }
+
+    //konstruktor
+    public Prostopadloscian(double h, double w, double l)
+    {
+        Height = h;
+        Width = w;
+        Length = l;
+    }
+
+    //metoda
+    public double Objentosc()
+    {
+        return Height*Width*Length;
+    }
+    
+    //metoda
+    public void Porownanie(double p1, double p2)
+    {
+        if(p1>p2)
+            Console.WriteLine("Prostopadloscian pierwszy jest wiekszy.");
+        else
+            Console.WriteLine("Prostopadloscian drugi jest wiekszy.");
+    }
+}
+
+
---
+++ Aplikacje Desktopowe/Lab_0_1/Zadanie2/Program.cs
@@ -0,0 +1,45 @@
+﻿class Punkt
+{
+    //właściwości
+    public int X { get; set; }
+    public int Y { get; set; }
+    //konstrutor
+    public Punkt(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    //metoda
+    public void Przesun(int vx, int vy)
+    {
+        X += vx;
+        Y += vy;
+    }
+
+    //metoda
+    public void Wyswietl()
+    {
+        Console.WriteLine($"x: {X}; y:{Y}");
+    }
+}
+
+class Start
+{
+    static void Main(string[] args)
+    {
+        //obiekt klasy punkt i wywołanie jego metod
+        Punkt pkt = new Punkt(5, 4);
+        pkt.Przesun(2, 1);
+        pkt.Wyswietl();
+
+        //2 obiekty klasy prostopadloscian
+        Prostopadloscian pros1 = new Prostopadloscian(4, 6, 5.5);
+        Prostopadloscian pros2 = new Prostopadloscian(5, 10, 12);
+
+        //wywołanie moetody 
+        pros1.Porownanie(pros1.Objentosc(), pros2.Objentosc());
+    }
+}
+
+
