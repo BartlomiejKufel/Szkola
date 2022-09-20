a7753dac8d7a0cc31d30b9dbff18b55ef5f9063a
Bartłomiej Kufel <joloczok123@gmail.com>
2022-09-20 09:37:32 +0200

    nowa apka

---
+++ Aplikacje Mobilne/NewControls/NewControls/NewControls/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewControls
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            var mySwitch = (sender as Switch);
+         
+            if (mySwitch.IsToggled)
+            {
+                switchLable.TextColor = Color.Blue;
+            }
+            else
+            {
+                switchLable.TextColor = Color.Green;
+            }
+            if(e.Value)
+            {
+                switchStateLable.Text = "ON";
+            }
+            else
+            {
+                switchStateLable.Text = "OFF";
+            }
+        }
+    }
+}
