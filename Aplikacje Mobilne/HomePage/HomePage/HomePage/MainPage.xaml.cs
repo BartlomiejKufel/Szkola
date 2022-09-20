70328a950e42c14fb05b261137c2843b81f5e942
Bartłomiej Kufel <joloczok123@gmail.com>
2022-09-20 08:12:12 +0200

    Dodanie Aplikacji Moblinych

---
+++ Aplikacje Mobilne/HomePage/HomePage/HomePage/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void addNumbers_Clicked(object sender, EventArgs e)
+        {
+            int sum = Convert.ToInt32(num2Entry.Text) + Convert.ToInt32(num1Entry.Text);
+            Wynik.Text=sum.ToString();
+        }
+    }
+}
