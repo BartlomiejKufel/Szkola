a5c1c4d21cf296cd210ca836238d236ebbdd1017
Bartłomiej Kufel <joloczok123@gmail.com>
2022-09-27 10:10:22 +0200

    Obliczanie równania kwadratowego
    Aplikacja Obliczająca równanie kwadratowe

--- Aplikacje Mobilne/NewControls/NewControls/NewControls/MainPage.xaml.cs
+++ Aplikacje Mobilne/NewControls/NewControls/NewControls/MainPage.xaml.cs
@@ -15,26 +15,58 @@ namespace NewControls
             InitializeComponent();
         }
 
-        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        private void runProgram_CheckedChanged(object sender, CheckedChangedEventArgs e)
         {
-            var mySwitch = (sender as Switch);
-         
-            if (mySwitch.IsToggled)
+            if (runProgram.IsChecked)
             {
-                switchLable.TextColor = Color.Blue;
+                programStack.IsVisible= true;
             }
             else
             {
-                switchLable.TextColor = Color.Green;
-            }
-            if(e.Value)
-            {
-                switchStateLable.Text = "ON";
-            }
-            else
-            {
-                switchStateLable.Text = "OFF";
+                programStack.IsVisible= false;
             }
         }
+        private void bSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var b = (int)bSlider.Value;
+            bLabel.Text = b.ToString();
+
+        }
+
+        private void aStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            aLabel.Text = aStepper.Value.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(!wynikEditor.IsVisible)
+                wynikEditor.IsVisible = true;
+
+            int a = (int)aStepper.Value;
+            int b = (int)bSlider.Value;
+            int c = int.Parse(cEntry.Text);
+
+            double delta = (b * b) - (4 * a * c);
+
+            if (delta > 0)
+            {
+                double x1 = Math.Round((-b - Math.Sqrt(delta)) / 2 * a, 2); 
+                double x2 = Math.Round((-b + Math.Sqrt(delta)) / 2 * a, 2);
+                
+                wynikEditor.Text = $"Delta: {delta}\nx1: {x1}\nx2: {x2}";
+
+            }
+            else if (delta == 0)
+            {
+                double x0 = -(b / 2 * a);
+                wynikEditor.Text = $"Delta: {delta}\nx0: {x0})";
+            }
+            else
+            {
+                wynikEditor.Text = $"Delta: {delta}\nRównanie nie posiada rozwiązania.";
+            }
+        }
+
     }
 }
--- Aplikacje Mobilne/NewControls/NewControls/NewControls/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ Aplikacje Mobilne/NewControls/NewControls/NewControls/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,16 +17,40 @@ namespace NewControls {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Label switchLable;
+        private global::Xamarin.Forms.CheckBox runProgram;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Label switchStateLable;
+        private global::Xamarin.Forms.StackLayout programStack;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Stepper aStepper;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label aLabel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Slider bSlider;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label bLabel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry cEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Editor wynikEditor;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            switchLable = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "switchLable");
-            switchStateLable = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "switchStateLable");
+            runProgram = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CheckBox>(this, "runProgram");
+            programStack = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "programStack");
+            aStepper = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Stepper>(this, "aStepper");
+            aLabel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "aLabel");
+            bSlider = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Slider>(this, "bSlider");
+            bLabel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "bLabel");
+            cEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "cEntry");
+            wynikEditor = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Editor>(this, "wynikEditor");
         }
     }
 }
