30467c697e9ae8baa3fdf3b976061e6434cc73ce
Bartłomiej Kufel <87674236+BartlomiejKufel@users.noreply.github.com>
2022-09-28 11:24:13 +0200

    zadanie

--- Aplikacje Webowe/express/start/app.js
+++ Aplikacje Webowe/express/start/app.js
@@ -10,6 +10,8 @@ app.get('/about', (req,res,next) =>{
     res.send('<h1>Uczymy się w technikum<h1/>')
 })
 
+
+
 app.use((req, res, next)=>{
     res.status(404);
     res.send('<h1>Page not found!</h1>')
