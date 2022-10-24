c9bbe229b0ac71611c9a621da04d4304c0dbc725
Bartłomiej Kufel <joloczok123@gmail.com>
2022-10-24 22:29:19 +0200

    Skończone Money console

--- Aplikacje Desktopowe/Pieniadze/Money_Consol/Money_Consol/Peopel.cs
+++ Aplikacje Desktopowe/Pieniadze/Money_Consol/Money_Consol/Peopel.cs
@@ -17,7 +17,26 @@ namespace Money_Consol
             Console.WriteLine($"{Name} ma {Money} zł");
         }
 
-        //Przyjmij Pieniądze
+        //jedna metoda na zklejona z 3 innych
+        public Person TakeMoney(Person per1, Person per2, int money)
+        {
+            int check = per2.Money - money;
+            if (check < 0)
+            {
+                Console.WriteLine($"{per2.Name} mówi: nie mam wystarczających środków, aby dać ci {money} zł.");
+            }
+            else if (money == 5)
+            {
+                per1.Money += money+10;
+                per2.Money -= money;
+            }
+            else
+            {
+                per1.Money += money;
+                per2.Money -= money;
+            }
 
+            return per1;
+        }
     }
 }
--- Aplikacje Desktopowe/Pieniadze/Money_Consol/Money_Consol/Program.cs
+++ Aplikacje Desktopowe/Pieniadze/Money_Consol/Money_Consol/Program.cs
@@ -7,27 +7,40 @@
             Person jacek = new Person() { Name="Jacek", Money=50};
             Person bartek = new Person() { Name = "Bartek", Money = 100 };
 
-            jacek.ShowInfo();
-            bartek.ShowInfo();
-            var moneyRequest="";
-            int money;
 
-            Console.Write("Podaj kwotę: ");
-            moneyRequest = Console.ReadLine();
-
-            if (String.IsNullOrEmpty(moneyRequest))
+            do
             {
-                Environment.Exit(0);   
-            }
-            else 
-            {
-                int.TryParse(moneyRequest, out money);
-            }
+                jacek.ShowInfo();
+                bartek.ShowInfo();
+                var moneyRequest="";
+                int money=0;
 
-            Console.WriteLine("Pieniądze ma przekazać:");
-            var personRequest = Console.ReadLine();
+                Console.Write("Podaj kwotę: ");
+                moneyRequest = Console.ReadLine();
 
+                if (String.IsNullOrEmpty(moneyRequest))
+                {
+                    Environment.Exit(0);   
+                }
+                else 
+                {
+                    int.TryParse(moneyRequest, out money);
+                }
+
+                Console.Write("Pieniądze ma przekazać: ");
+                var personRequest = Console.ReadLine();
+
+                switch (personRequest)
+                {
+                    case "Bartek": jacek.TakeMoney(jacek, bartek, money); break;
+                    case "Jacek": bartek.TakeMoney(bartek, jacek, money);break;
+                }
+
+            }while (true);
+        
+            
         }
+
 
 
 
