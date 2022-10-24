ba670265bf01e29de1c4390f0f1cf1bb11114da1
Bartłomiej Kufel <joloczok123@gmail.com>
2022-10-24 16:18:50 +0200

    Dodanie Pieniądze

---
+++ Aplikacje Desktopowe/Pieniadze/Money_Consol/Money_Consol/Peopel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Consol
+{
+    public class Person
+    {
+        public string? Name;
+        public int Money;
+
+        //Wypisz Info
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} ma {Money} zł");
+        }
+
+        //Przyjmij Pieniądze
+
+    }
+}
---
+++ Aplikacje Desktopowe/Pieniadze/Money_Consol/Money_Consol/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Money_Consol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person jacek = new Person() { Name="Jacek", Money=50};
+            Person bartek = new Person() { Name = "Bartek", Money = 100 };
+
+            jacek.ShowInfo();
+            bartek.ShowInfo();
+            var moneyRequest="";
+            int money;
+
+            Console.Write("Podaj kwotę: ");
+            moneyRequest = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(moneyRequest))
+            {
+                Environment.Exit(0);   
+            }
+            else 
+            {
+                int.TryParse(moneyRequest, out money);
+            }
+
+            Console.WriteLine("Pieniądze ma przekazać:");
+            var personRequest = Console.ReadLine();
+
+        }
+
+
+
+
+    }
+}
