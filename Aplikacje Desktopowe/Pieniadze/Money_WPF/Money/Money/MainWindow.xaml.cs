6f889daacac508502d647eeac622ac94701f2828
Bartłomiej Kufel <joloczok123@gmail.com>
2022-10-27 08:29:29 +0200

    Skończony projekt wpf Money

--- Aplikacje Desktopowe/Pieniadze/Money_WPF/Money/Money/MainWindow.xaml.cs
+++ Aplikacje Desktopowe/Pieniadze/Money_WPF/Money/Money/MainWindow.xaml.cs
@@ -17,14 +17,55 @@ namespace Money
 {
     public partial class MainWindow : Window
     {
+        Person jacek = new Person() { Name = "Jacek", Money = 50 };
+        Person bartek = new Person() { Name = "Bartek", Money = 100 };
+        Person command = new Person();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            command.ShowInfo(jacek, bartek);
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_Jacek(object sender, RoutedEventArgs e)
         {
+            ErrorTextBlock.Text = "";
+            var money =0;
+            int.TryParse(moneyTextBox.Text.Trim(), out money);
 
+            int tmp = jacek.GiveMoney(money, jacek);
+            if (tmp == 0)
+            {
+                ErrorTextBlock.Text = $"{jacek.Name} mówi: nie mam wystarczająco środków aby dać ci {money} zł.";
+            }
+            else
+            {
+                bartek.TakeMoney(tmp, bartek);
+            }
+
+            command.ShowInfo(jacek, bartek);
+        }
+
+
+        private void Button_Click_Bartek(object sender, RoutedEventArgs e)
+        {
+            ErrorTextBlock.Text = "";
+            var money = 0;
+            int.TryParse(moneyTextBox.Text.Trim(), out money);
+
+            int tmp = bartek.GiveMoney(money, bartek);
+            if (tmp == 0)
+            {
+                ErrorTextBlock.Text=$"{bartek.Name} mówi: nie mam wystarczająco środków aby dać ci {money} zł.";
+            }
+            else
+            {
+                jacek.TakeMoney(tmp, jacek);
+            }
+
+            command.ShowInfo(jacek, bartek);
         }
     }
 }
---
+++ Aplikacje Desktopowe/Pieniadze/Money_WPF/Money/Money/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Money
+{
+    public class Person
+    {
+        public string? Name;
+        public int Money;
+
+        
+
+        public void ShowInfo(Person person1, Person person2)
+        {
+            MainWindow wnd = (MainWindow)Application.Current.MainWindow;
+            wnd.moneyJacekLable.Text = $"{person1.Money} zł";
+            wnd.moneyBartekLable.Text = $"{person2.Money} zł";
+        }
+        public Person TakeMoney(int money, Person per)
+        {
+            per.Money += money;
+            return per;
+        }
+
+        public int GiveMoney(int money, Person per)
+        {
+            int check = Money - money;
+            if (check < 0 || money < 0)
+            {
+                return 0;
+            }
+            else if (CheckForBonus(money))
+            {
+                per.Money -= money;
+                return money + 10;
+            }
+            else
+            {
+                per.Money -= money;
+                return money;
+            }
+        }
+
+        public static bool CheckForBonus(int money)
+        {
+            if (money == 5)
+                return true;
+            else
+                return false;
+
+        }
+
+
+
+    }
+}
