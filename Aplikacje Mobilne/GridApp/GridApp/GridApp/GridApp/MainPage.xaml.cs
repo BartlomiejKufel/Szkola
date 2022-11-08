906493448c39ae0d8f6b61728bbaa2f4ea3dfefc
Bartłomiej Kufel <joloczok123@gmail.com>
2022-11-08 09:30:53 +0100

    Skończony program

--- Aplikacje Mobilne/GridApp/GridApp/GridApp/GridApp/MainPage.xaml.cs
+++ Aplikacje Mobilne/GridApp/GridApp/GridApp/GridApp/MainPage.xaml.cs
@@ -10,9 +10,123 @@ namespace GridApp
 {
     public partial class MainPage : ContentPage
     {
+        private float number1;
+        private float number2;
+        private bool isNumberSecond = false;
+        private string operation;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void NumberButton_Clicked(object sender, EventArgs e)
+        {
+            if (poleEntry.Text =="0")
+            {
+                poleEntry.Text = string.Empty;
+            }
+            
+
+            var sen = sender as Button;
+            poleEntry.Text += sen.Text;
+        }
+
+        private void NegativePositiveButton_Clicked(object sender, EventArgs e)
+        {
+            string entry = poleEntry.Text;
+            
+            if (!entry.StartsWith("-") && !(entry=="0"))
+            {
+                poleEntry.Text = $"-{entry}";
+            }
+            else if (entry =="0")
+            {
+                poleEntry.Text = entry;
+            }
+            else
+            {
+                poleEntry.Text = $"{entry.Substring(1,entry.Length-1)}";
+            }
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            poleEntry.Text = "0";
+            isNumberSecond = false;
+            operation = "";
+            number1 = 0;
+            number2 = 0;
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            string primalEntry = poleEntry.Text;
+            string subEntry = primalEntry.Substring(0,primalEntry.Length-1);
+            if (subEntry == string.Empty)
+            {
+                poleEntry.Text = "0";
+            }
+            else
+            {
+                poleEntry.Text= subEntry;
+            }
+        }
+
+        private void OperationButton_Clicked(object sender, EventArgs e)
+        {
+            var sen = sender as Button;
+
+            float.TryParse(poleEntry.Text, out number1);
+
+            isNumberSecond = true;
+
+            poleEntry.Text = "0";
+
+            operation = sen.Text;
+        }
+
+        private void ResultButton_Clicked(object sender, EventArgs e)
+        {
+            
+            float.TryParse(poleEntry.Text, out number2);
+
+            float result = 0;
+            switch (operation)
+            {
+                case "+":result = number1 + number2;break; 
+                case "-":result = number1 - number2;break; 
+                case "*":result = number1 * number2;break; 
+                default:
+                    break;
+            }
+
+            if (operation == "/" && number2 != 0) result = number1 / number2;
+
+            poleEntry.Text = $"{result}";
+            operation = "";
+            isNumberSecond = false;
+        }
+
+        private void FractionButton_Clicked(object sender, EventArgs e)
+        {
+            string entry = poleEntry.Text;
+            if (!entry.Contains("."))
+            {
+                poleEntry.Text+=".";
+            }
+        }
+
+        private void PercentButton_Clicked(object sender, EventArgs e)
+        {
+            float percentNum = 0;
+            float.TryParse(poleEntry.Text, out percentNum);
+
+            if (isNumberSecond)
+            {
+                 percentNum= (number1*percentNum)/1000;
+            }
+            poleEntry.Text = percentNum.ToString();
         }
     }
 }
--- Aplikacje Mobilne/GridApp/GridApp/GridApp/GridApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ Aplikacje Mobilne/GridApp/GridApp/GridApp/GridApp/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace GridApp {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry poleEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            poleEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "poleEntry");
         }
     }
 }
