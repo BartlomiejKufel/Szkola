c3aea15d2475a06aee15210bbfb6be71dd4854cc
Bartłomiej Kufel <joloczok123@gmail.com>
2022-11-10 07:29:34 +0100

    dodanie WPF_GAME_CONSOLA

---
+++ Aplikacje Desktopowe/WPF_GRA_CONSOLA/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game();
+        }
+
+
+
+        public static void Game()
+        {
+            Random rnd=new Random();
+            int numberToFind=rnd.Next(1,11);
+
+            while(true)
+            {
+                Console.WriteLine("Podaj liczbę z zakresu od 1 do 10");
+                int tmp = 0;
+                int.TryParse(Console.ReadLine(), out tmp);
+
+                if (tmp == numberToFind)
+                {
+                    break;
+                }
+                else if(tmp > numberToFind)
+                    Console.WriteLine("mniej\n");
+                else if(tmp < numberToFind)
+                    Console.WriteLine("więcej\n");
+            
+            }
+
+            Console.WriteLine("Gratulacje");
+        }
+    }
+}
