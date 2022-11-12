259d896d02f7a00202f7fcc11c6f3040027021d2
Bartłomiej Kufel <joloczok123@gmail.com>
2022-11-12 15:53:24 +0100

    Dodanie Algorytmu na Silnię liczby naturalnej

---
+++ Aplikacje Desktopowe/Projekt GPR/BartlomiejKufel/BartlomiejKufel/Algorithms1-4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BartlomiejKufel
+{
+    partial class Algorithms
+    {
+        //Algorytm 2
+        public static void FactorialOfNumber()
+        {
+            Console.Clear();
+
+            int primalNumber;
+            int result=1;
+            Console.WriteLine("Podaj liczbę naturalną");
+            int.TryParse(Console.ReadLine(), out primalNumber);
+
+            for (int i = 1; i <= primalNumber; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine($"\nSilnią liczby {primalNumber} jest {result}");
+
+            Menu.ExitAlgoritm();
+        }
+
+
+    }
+}
--- Aplikacje Desktopowe/Projekt GPR/BartlomiejKufel/BartlomiejKufel/Menu.cs
+++ Aplikacje Desktopowe/Projekt GPR/BartlomiejKufel/BartlomiejKufel/Menu.cs
@@ -50,7 +50,7 @@
                 switch (option)
                 {
                     case 1:; break;
-                    case 2:; break;
+                    case 2: Algorithms.FactorialOfNumber(); break;
                     case 3:; break;
                     case 4:; break;
                     case 5:; break;
