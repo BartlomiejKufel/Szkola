d18deee3d228da7cf3d3acaf58dcadbcadfbab32
Bartłomiej Kufel <joloczok123@gmail.com>
2022-11-30 13:39:34 +0100

    Dodanie ConsolApp WPF_YT

---
+++ Aplikacje Desktopowe/WPF_YT/ConsoleApp/ConsoleApp/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class User
+    {
+        public double Portfel { get; set; }
+
+
+        public User(double portfel)
+        {
+            Portfel=portfel;
+        }
+
+        public void ShowMoney()
+        {
+            Console.WriteLine($"Gracz ma {Portfel} zł");
+        }
+    }
+}
