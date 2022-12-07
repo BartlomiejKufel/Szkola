c3bc411508a2155a6793b31da1d9da3c6297950b
Bartłomiej Kufel <joloczok123@gmail.com>
2022-12-07 13:41:03 +0100

    Początek c#_car-dog

---
+++ Aplikacje Desktopowe/c#_car-dog/car/car/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Aplikacje Desktopowe/c#_car-dog/car/car/Samochod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    public class Samochod
+    {
+        private string marka;
+        private double poj_baku;
+        private int predkosc_max;
+        private double zuzycie_paliwa;
+
+        public Samochod(string marka, double poj_baku, int predkosc_max, double zuzycie_paliwa)
+        {
+            this.marka = marka;
+            this.poj_baku = poj_baku;
+            this.predkosc_max = predkosc_max;
+            this.zuzycie_paliwa = zuzycie_paliwa;
+        }
+
+        public void Jedz(double jakSzybko, double jakDaleko)
+        {
+
+        }
+        
+
+
+    }
+}
