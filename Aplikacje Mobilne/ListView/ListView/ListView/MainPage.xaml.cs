03f6aca435a3ccac7e0cc563363c586ac5ea909e
Bartłomiej Kufel <joloczok123@gmail.com>
2022-12-13 10:08:31 +0100

    add ListView

---
+++ Aplikacje Mobilne/ListView/ListView/ListView/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListView
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<User> users;
+        public MainPage()
+        {
+            InitializeComponent();
+           users = new List<User>()
+           {
+               new User(){Name="Bartłomiej Kufel", Email="kufel.bartlomiej@google.com" },
+               new User(){Name="Andrzej Kowalski", Email="kowalski.andrzej@wp.pl" },
+               new User(){Name="Piotr Szalik", Email="szalik.piotr@onet.pl" },
+           };
+
+           usersListView.ItemsSource = users;
+        }
+
+        private void addUser_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
