4888388d1dbc8064ed48495668f36ceda2d4460b
Bartłomiej Kufel <joloczok123@gmail.com>
2022-12-14 13:52:55 +0100

    eddit

--- Aplikacje Desktopowe/c#_car-dog/dogs/dogs/MainWindow.xaml.cs
+++ Aplikacje Desktopowe/c#_car-dog/dogs/dogs/MainWindow.xaml.cs
@@ -20,16 +20,76 @@ namespace dogs
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool status = false;
+        private Person bartek = new Person("Bartek", 100, 0, 0);
+        private Person janek = new Person("Janek", 100, 0, 0);
+        private Person antek = new Person("Antek", 100, 0, 0);
         public MainWindow()
         {
             InitializeComponent();
+            janekRadio.Content = $"{janek.Name} ma {janek.money} zł";
+
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            var radio = sender as RadioButton;
-            string? name = radio.Content.ToString();
-            personLable.Text = name;
+            var radio = (RadioButton)sender;
+            var name = radio.Content.ToString();
+            switch (name[0].ToString())
+            {
+                case "B": personLable.Text = "Bartek";break;
+                case "J": personLable.Text = "Janek";break;
+                case "A": personLable.Text = "Antek";break;
+
+                default:
+                    break;
+            }
+
+            submitButton.IsEnabled = true;
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string person = personLable.Text;
+            if (!status)
+            {
+                switch (person)
+                {
+                    case "Bartek": bartekTextBox.Text = "Bartek nie zawarł zakładu"; break;
+                    case "Janek": janekTextBox.Text = "Janek nie zawarł zakładu"; break;
+                    case "Antek": antekTextBox.Text = "Antek nie zawarł zakładu"; break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+
+
+
+            }
+
+
+        }
+
+        private void ButtonStatus_Click(object sender, RoutedEventArgs e)
+        {
+            var sen = (Button)sender;
+            if (status)
+            {
+                sen.Content = "nie stawia";
+                dogNumberInput.IsEnabled = false;
+                priceInput.IsEnabled = false;
+                status = false;
+            }
+            else
+            {
+                sen.Content = "stawia";
+                dogNumberInput.IsEnabled = true;
+                priceInput.IsEnabled = true;
+                status = true;
+            }
         }
     }
 }
---
+++ Aplikacje Desktopowe/c#_car-dog/dogs/dogs/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dogs
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int money { get; set; }
+        public int DogNumber { get; set; }
+        public int Bet { get; set; }
+
+        public Person(string name, int money, int dogNumber, int bet)
+        {
+            Bet = bet;
+            Name = name;
+            this.money = money;
+            this.DogNumber = dogNumber;
+        }
+    }
+}
