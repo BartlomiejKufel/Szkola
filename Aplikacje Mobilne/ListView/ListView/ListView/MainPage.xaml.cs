2d57d7d9b8c5acff65c5a5f8d83008e226cdc9e9
Bartłomiej Kufel <joloczok123@gmail.com>
2023-01-03 10:10:52 +0100

    add

--- Aplikacje Mobilne/ListView/ListView/ListView/MainPage.xaml.cs
+++ Aplikacje Mobilne/ListView/ListView/ListView/MainPage.xaml.cs
@@ -35,5 +35,66 @@ namespace ListView
 
             users.Add(new User() { Name = enteredName, Email = enteredEmail });
         }
+
+        private async void moreMenuItem_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+            var user = (User)menuItem.CommandParameter;
+            var userDetails = $"Nazwa: {user.Name}\nEmail: {user.Email}"; 
+            await DisplayAlert("Informacje o urzytkowniku", userDetails, "Ok");
+        }
+
+        private async void deleteMenuItem_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+            var user = (User)menuItem.CommandParameter;
+
+            var delete = await DisplayAlert($"Usuwanie użytkownika: {user.Name}", "Czy napewno chcesz usunąć użytkownika", "Tak", "Nie");
+            if (!delete) return;
+
+            var confirm = users.Remove(user);
+
+            string msg = "";
+            if (confirm)
+                msg = "Pomyślnie usunięto użytkownika.";
+            else
+                msg = "Wystąpił błąd podczas usuwania użytkownika";
+
+            await DisplayAlert("Informacja", msg, "OK");
+           
+        }
+
+        private async void editMenuItem_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+            var user = (User)menuItem.CommandParameter;
+
+            var find = users.IndexOf(user);
+            var editName = "";
+            do
+            {
+                editName = await DisplayPromptAsync("", "Edytuj Imię i Nazwisko", "OK", "Anuluj", user.Name);
+                if (editName == null) return;
+            } while (editName == String.Empty);
+            user.Name = editName;
+
+            var editEmail = "";
+            do
+            {
+                editEmail = await DisplayPromptAsync("", "Edytuj Email", "OK", "Anuluj", user.Email);
+                if (editEmail == null) return;
+            }while (editEmail == String.Empty);
+
+            user.Email = editEmail;
+
+            users[find] = user;
+
+        }
+
+        private void usersListView_Refreshing(object sender, EventArgs e)
+        {
+            usersListView.ItemsSource = users;
+            usersListView.EndRefresh();
+        }
     }
 }
