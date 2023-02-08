2285131da9356b5c477fe2b62c63ba10b0ef421c
Bartłomiej Kufel <joloczok123@gmail.com>
2023-02-08 10:25:34 +0100

    test

---
+++ Aplikacje Mobilne/TestListV/Test/Test/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+
+namespace KufelBartlomiej
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Tasks> tasks;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            tasks = new ObservableCollection<Tasks>()
+            {
+                new Tasks(){ Name = "Spotkanie biznesowe", IsImportant=true},
+                new Tasks(){ Name = "Nauka do pana B.", IsImportant=false}
+            };
+
+            taskListView.ItemsSource = tasks;
+        }
+
+        private void taskListView_Refreshing(object sender, EventArgs e)
+        {
+            taskListView.ItemsSource = tasks;
+            taskListView.EndRefresh();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string name = taskNameEntry.Text;
+            taskNameEntry.Text = "";
+
+            bool isImportant = importantSwitch.IsToggled;
+            importantSwitch.IsToggled= false;
+
+            tasks.Add(new Tasks() { Name=name, IsImportant=isImportant});
+            taskListView.ItemsSource = tasks;
+        }
+
+        private async void Delete_Clicked(object sender, EventArgs e)
+        {
+            var menu = (MenuItem)sender;
+            var task = (Tasks)menu.CommandParameter;
+
+            var delete = await DisplayAlert($"Usuwanie zadania: {task.Name}", "Czy napewno chcesz usunąć zadanie", "Tak", "Nie");
+            if (!delete) return;
+
+            var confirm = tasks.Remove(task);
+
+            string msg = "";
+            if (confirm)
+                msg = "Pomyślnie usunięto zadanie";
+            else
+                msg = "Wystąpił błąd";
+
+            await DisplayAlert("Wiadomość", msg, "OK");
+
+            taskListView.ItemsSource = tasks;
+        }
+    }
+}
---
+++ Aplikacje Mobilne/TestListV/Test/Test/Tasks.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KufelBartlomiej
+{
+    internal class Tasks
+    {
+        public string Name { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
