32160586712ab1005bc136a9b744fd400d340d85
Bartłomiej Kufel <joloczok123@gmail.com>
2023-02-14 10:08:59 +0100

    projekt

---
+++ Aplikacje Mobilne/Rachunek/Rachunek/Rachunek/BillPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Rachunek
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BillPage : ContentPage
+    {
+        public BillPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
