0d9c9dd8624d403cad200079ab39955803d0790d
Bartłomiej Kufel <joloczok123@gmail.com>
2023-02-15 13:45:07 +0100

    binary

---
+++ Aplikacje Desktopowe/BinaryNum/BinaryNum/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BinaryNum
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(inputTextBox.Text, out int num);
+            string binary = "";
+
+            while (num!=0)
+            {
+                binary += num % 2;
+                num /= 2;
+            }
+
+            string reverse = String.Empty;
+            
+            for (int i = binary.Length - 1; i > -1; i--)
+            {
+                reverse += binary[i];
+            }
+
+
+            outputTextBox.Text = reverse;
+        }
+    }
+}
