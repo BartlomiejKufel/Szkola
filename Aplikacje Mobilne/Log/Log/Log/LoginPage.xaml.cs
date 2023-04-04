b168bd9953f0818aeb3831c03f218658c29ae3fe
Bartłomiej Kufel <joloczok123@gmail.com>
2023-04-04 08:17:54 +0200

    g

---
+++ Aplikacje Mobilne/Log/Log/Log/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Log
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void newAccountButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewAccPage());
+
+        }
+    }
+}
