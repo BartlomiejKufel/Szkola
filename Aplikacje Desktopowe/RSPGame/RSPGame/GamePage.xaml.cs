6fd6373fa20d28f3104f7379f48700d6c3bd7854
Bartłomiej Kufel <joloczok123@gmail.com>
2023-04-13 09:28:54 +0200

    game end
    Może kiedyś dokończ

--- Aplikacje Desktopowe/RSPGame/RSPGame/GamePage.xaml.cs
+++ Aplikacje Desktopowe/RSPGame/RSPGame/GamePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace RSPGame
 {
@@ -48,7 +49,119 @@ namespace RSPGame
                 scissorsButton.BorderBrush = Brushes.Transparent;
             }
 
-            button.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#26bf3d"); ;
+            button.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#26bf3d");
+            gameStartButton.IsEnabled = true;
+        }
+
+        private async void GameStart_Click(object sender, RoutedEventArgs e)
+        {
+            botStackPanel.Visibility = Visibility.Collapsed;
+            Animation();
+            await Task.Delay(3500);
+
+            Random rnd = new Random();
+
+            string path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).FullName;
+
+
+            int los = rnd.Next(1,4);
+                botStackPanel.Visibility = Visibility.Visible;
+            if (los == 1)
+            {
+                botImage.Source = new BitmapImage(new Uri(System.IO.Path.Combine(path, @"\images\rock.png"), UriKind.Relative));
+            }
+            else if (los == 2)
+            {
+                botImage.Source = new BitmapImage(new Uri(System.IO.Path.Combine(path, @"\images\paper.png"), UriKind.Relative));
+            }
+            else if (los == 3)
+            {
+                botImage.Source = new BitmapImage(new Uri(System.IO.Path.Combine(path, @"\images\scissors.png"), UriKind.Relative));
+            }
+
+            MessageBox.Show(CheckForWin(los));
+
+        }
+
+
+        private async void Animation()
+        {
+            animationStackPanel.Visibility = Visibility.Visible;
+            dotsTextBlock.Text = ".";
+            for (int i = 0; i < 8; i++)
+            {
+
+                if (dotsTextBlock.Text == ". . .")
+                {
+                    dotsTextBlock.Text = ".";
+                    await Task.Delay(300);
+                }
+
+                dotsTextBlock.Text += " .";
+                await Task.Delay(300);
+            }
+
+            animationStackPanel.Visibility = Visibility.Collapsed;
+        }
+
+        //1-kamień
+        //2-papier
+        //3-nożyczki
+        
+        private string CheckForWin(int los)
+        {
+            switch (los)
+            {
+                case 1:
+                    if (choice == "scissorsButton")
+                    {
+                        return "Przegrałeś";
+                    }
+                    else if (choice == "rockButton")
+                    {
+                        return "Remis";
+                    }
+                    else if (choice == "paperButton")
+                    {
+                        return "Wygrałeś";
+                    }
+                    break;
+
+                case 2:
+                    if (choice == "scissorsButton")
+                    {
+                        return "Wygrałeś";
+                    }
+                    else if (choice == "rockButton")
+                    {
+                        return "Przegrałeś";
+                    }
+                    else if (choice == "paperButton")
+                    {
+                        return "Remis";
+                    }
+                    break;
+
+                case 3:
+                    if (choice == "scissorsButton")
+                    {
+                        return "Remis";
+                    }
+                    else if (choice == "rockButton")
+                    {
+                        return "Wygrałeś";
+                    }
+                    else if (choice == "paperButton")
+                    {
+                        return "Przegrałeś";
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            return "lose";
         }
     }
 }
--- Aplikacje Desktopowe/RSPGame/RSPGame/obj/Debug/net6.0-windows/App.g.cs
+++ Aplikacje Desktopowe/RSPGame/RSPGame/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace RSPGame {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace RSPGame {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             RSPGame.App app = new RSPGame.App();
             app.InitializeComponent();
