75af089c845c5096052d567b9b6303f1c84382b0
Bartłomiej Kufel <joloczok123@gmail.com>
2023-05-30 09:36:18 +0200

    search_bar work

--- Aplikacje Mobilne/DbApp/DbApp/DbApp/MainPage.xaml.cs
+++ Aplikacje Mobilne/DbApp/DbApp/DbApp/MainPage.xaml.cs
@@ -65,5 +65,34 @@ namespace DbApp
 
             await readDatabase();
         }
+
+        
+
+        private async void emailSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SearchBar searchBar = sender as SearchBar;
+            var searchEmail = searchBar.Text;
+            if (searchEmail != "")
+            {
+                List<Contact> contacts = new List<Contact>();
+                var connection = new SQLiteAsyncConnection(App.GetDbPath());
+
+                await connection.CreateTableAsync<Contact>();
+
+                contacts = await connection.Table<Contact>().ToListAsync();
+
+                await connection.CloseAsync();
+
+                List<Contact> filteredContacts = new List<Contact>();
+
+                filteredContacts = contacts.Where(contact => contact.Email.StartsWith(searchEmail)).ToList();
+
+                contactsListView.ItemsSource = filteredContacts;
+            }
+            else
+            {
+                await readDatabase();
+            }
+        }
     }
 }
